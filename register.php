bd740eab520450469fc4a99672b7727660b5a438
kamwinemercy705-bytech <kamwinemercy705@gmail.com>
2026-08-30 09:43:32 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,25 +1,25 @@
 
 <?php
 
-
 header("Access-Control-Allow-Origin: https://crown-cash.vercel.app");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 
-
+/*
+|--------------------------------------------------------------------------
+| CORS preflight
+|--------------------------------------------------------------------------
+*/
 
 if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
-
     http_response_code(204);
     exit;
 }
 
 require_once __DIR__ . "/config.php";
 
-header("Content-Type: application/json; charset=UTF-8");
 
-require_once __DIR__ . "/config.php";
 
 
 /*
